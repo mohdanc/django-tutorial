dcea72ee415e4773301b589bab84ed4ae363bd1b
Ans <m.ans@devsinc.com>
2023-11-22 01:06:28 +0500

    add: models - register them in admin

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,24 @@
+'''Polls models'''
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Question(models.Model):
+    '''Question model'''
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField(verbose_name="date published")
+
+    def was_published_recently(self):
+        '''aaa'''
+        return self.pub_date >= timezone.now() - timezone.timedelta(days=1)
+
+    def __str__(self) -> str:
+        return str(self.question_text)
+
+class Choice(models.Model):
+    '''Choice model'''
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self) -> str:
+        return str(self.choice_text)
